ba21f8b164c27d651dd8fcaca92e7db6fc00ef07
eakarpov <eakarpov@yandex.ru>
2019-03-19 16:35:03 +0300

    windows script 2

--- windows/init-project.bat
+++ windows/init-project.bat
@@ -1,22 +1,32 @@
 del /q "%1\*"
 for /D %%p in ("%1\*.*") do rmdir "%%p" /s /q
 
-:: npx create-react-app $1 --typescript
-:: npm i graphlabs.core.template
-:: npm i --save-dev tslint tslint-react tslint-config-prettier
-:: # To be removed
-:: npm i --save-dev es6-promise redux-devtools-extension
+for /f "delims=" %%A in ("%1") do (
+   set foldername=%%~nxA
+)
 
-cp "..\sources\init-project\.editorconfig" "%1\.editorconfig"
-cp "../sources\init-project\tslint.json %1\tslint.json"
-cp "..\sources\init-project\tsconfig.json" "%1\tsconfig.json"
-cp "..\sources\init-project\App.tsx" "%1\src/App.tsx"
+set path = echo ("%cd")
+cd "%1"
 
-echo -e "//tslint:disable\n$(cat %1\src\service-worker.ts)" >> "%1\src\service-worker.ts"
-echo -e ".idea\n$(cat %1\.gitignore)" >> "%1\.gitignore"
+rename . test
 
-set tempfile = "%1\package.tmp"
-set inputfile = "%1\package.json"
+npx create-react-app $1 --typescript
+npm i graphlabs.core.template
+npm i --save-dev tslint tslint-react tslint-config-prettier
+# To be removed
+npm i --save-dev es6-promise redux-devtools-extension
+
+copy "%path%\sources\init-project\.editorconfig" ".\.editorconfig"
+copy "%path\sources\init-project\tslint.json .\tslint.json"
+copy "%path\sources\init-project\tsconfig.json" ".\tsconfig.json"
+copy "%path\sources\init-project\App.tsx" ".\src/App.tsx"
+
+echo -e "//tslint:disable\n$(cat %1\src\service-worker.ts)" >> ".\src\service-worker.ts"
+echo -e ".idea\n$(cat %1\.gitignore)" >> ".\.gitignore"
+
+set tempfile = ".\package.tmp"
+set inputfile = ".\package.json"
+copy /y nul %tempfile%
 
 for /F "eol= delims=" %l in (%inputfile%) do (
     if %%l == "\"scripts\": {" (
@@ -27,3 +37,7 @@ for /F "eol= delims=" %l in (%inputfile%) do (
 
 del /q "%inputfile%"
 move "%tempfile%" "%inputfile%"
+
+rename . %foldername%
+
+cd "%path%"
