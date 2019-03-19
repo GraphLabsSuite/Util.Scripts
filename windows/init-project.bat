9296311a642f941bd234cf3bc1b59b19a04c550e
eakarpov <eakarpov@yandex.ru>
2019-03-19 16:42:22 +0300

    some attempts

--- windows/init-project.bat
+++ windows/init-project.bat
@@ -7,25 +7,26 @@ for /f "delims=" %%A in ("%1") do (
 
 set path = echo ("%cd")
 cd "%1"
+cd "..\"
+set tempname = echo "graphlabs.temp.name"
+rename ".\%foldername%" "%tempname%"
 
-rename . test
-
-npx create-react-app $1 --typescript
+npx create-react-app %foldername% --typescript
 npm i graphlabs.core.template
 npm i --save-dev tslint tslint-react tslint-config-prettier
 # To be removed
 npm i --save-dev es6-promise redux-devtools-extension
 
-copy "%path%\sources\init-project\.editorconfig" ".\.editorconfig"
-copy "%path\sources\init-project\tslint.json .\tslint.json"
-copy "%path\sources\init-project\tsconfig.json" ".\tsconfig.json"
-copy "%path\sources\init-project\App.tsx" ".\src/App.tsx"
+copy "%path%\sources\init-project\.editorconfig" ".\%tempname%\.editorconfig"
+copy "%path\sources\init-project\tslint.json .\%tempname%\tslint.json"
+copy "%path\sources\init-project\tsconfig.json" ".\%tempname%\tsconfig.json"
+copy "%path\sources\init-project\App.tsx" ".\%tempname%\src\App.tsx"
 
-echo -e "//tslint:disable\n$(cat %1\src\service-worker.ts)" >> ".\src\service-worker.ts"
-echo -e ".idea\n$(cat %1\.gitignore)" >> ".\.gitignore"
+echo -e "//tslint:disable\n$(cat .\%tempname%\src\service-worker.ts)" >> ".\%tempname%\src\service-worker.ts"
+echo -e ".idea\n$(cat .\%tempname%\.gitignore)" >> ".\%tempname%\.gitignore"
 
-set tempfile = ".\package.tmp"
-set inputfile = ".\package.json"
+set tempfile = ".\%tempname%\package.tmp"
+set inputfile = ".\%tempname%\package.json"
 copy /y nul %tempfile%
 
 for /F "eol= delims=" %l in (%inputfile%) do (
@@ -38,6 +39,6 @@ for /F "eol= delims=" %l in (%inputfile%) do (
 del /q "%inputfile%"
 move "%tempfile%" "%inputfile%"
 
-rename . %foldername%
+rename ".\%tempname%" "%foldername%"
 
 cd "%path%"
